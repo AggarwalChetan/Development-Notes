e3c298f355d92455f3f23d273535baa88c9aaff9
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 20:15:08 +0530

    Revision string.js

--- Revision/intro.js
+++ Revision/intro.js
@@ -35,3 +35,29 @@ for(let i = 1; i <= 10; ++ i){
     console.log("Number is ", i);
 }
 
+// to print
+// multiple lines
+// System.out.println() -> console.log();
+// single line
+// System.out.print() -> process.stdout.write()
+
+// you will always get input in the form of string
+
+// input type cohersion
+// comparison, arithmetic -> (>, = , <), * /, => string will be converted into number
+let input = "10";
+let b = 10;
+console.log(input < 4);
+// concantenation -> string + number -> string
+console.log("Hello i am something" + 10);
+console.log("Hello i am something", 10);
+
+let ans = Number(input) + b;
+console.log(ans);
+
+
+
+
+
+
+
---
+++ Revision/strings.js
@@ -0,0 +1,11 @@
+let a = 10;
+
+let singleQuotesString = 'I am a single quotes string ' + a;
+let doubleQuotesString = "I am a double quotes string " + a;
+
+// Multiple Line String create
+let templateString = `I am a template string ${a}`;
+
+console.log(singleQuotesString);
+console.log(doubleQuotesString);
+console.log(templateString);
