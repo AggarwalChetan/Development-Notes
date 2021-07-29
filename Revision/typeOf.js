92efcd684a8d31f6f01fadc0214fa78a24f33c7a
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 21:43:46 +0530

    JSON.parse() added

--- Revision/typeOf.js
+++ Revision/typeOf.js
@@ -18,3 +18,10 @@ a = null;
 
 // null
 console.log(typeof a);
+
+// input -> manage
+// JSON.parse() is used to convert array in form of string i.e. "[1,4,5,6]" to array
+let str = "[1,2,5,5,3]";
+console.log(str, typeof str);
+let arr = JSON.parse(str);
+console.log(arr, Array.isArray(arr)); 
