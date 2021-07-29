b2cf20a6dcf2f16c5e353b0e89c795fa388d8332
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 21:36:58 +0530

    typeOf file added

---
+++ Revision/typeOf.js
@@ -0,0 +1,20 @@
+// type of
+let a = 10;
+
+// number
+console.log(typeof a);
+
+a = "vfdelbmkdgbm ";
+
+// string
+console.log(typeof a);
+
+a = true;
+
+// boolean
+console.log(typeof a);
+
+a = null;
+
+// null
+console.log(typeof a);
