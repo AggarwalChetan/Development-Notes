30226430179f440180bc182596cc60faa824b56e
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 18:25:13 +0530

    semi-colon added

--- Module_1_FS/Lecture2/activity/commands/help.js
+++ Module_1_FS/Lecture2/activity/commands/help.js
@@ -2,8 +2,8 @@
 function fn(props){
     console.log("List of all commands");
     console.log("1. node main.js tree " + props);
-    console.log("2. node main.js organize " + props)
-    console.log("3. node main.js help")
+    console.log("2. node main.js organize " + props);
+    console.log("3. node main.js help");
 }
 
 module.exports = {
