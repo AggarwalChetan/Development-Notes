0099eb851d2caa3f6bed67d1f278562a25a3456a
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 20:26:33 +0530

    String function added

--- Revision/strings.js
+++ Revision/strings.js
@@ -1,11 +1,36 @@
 let a = 10;
 
 let singleQuotesString = 'I am a single quotes string ' + a;
+console.log(singleQuotesString);
+
 let doubleQuotesString = "I am a double quotes string " + a;
+console.log(doubleQuotesString);
 
 // advantage -> Multiple Line String create
 let templateString = `I am a template string ${a}`;
-
-console.log(singleQuotesString);
-console.log(doubleQuotesString);
 console.log(templateString);
+
+// Length of String
+let length = templateString.length;
+console.log(length);
+
+// substring
+let substr = templateString.substring(2, 4);
+console.log(substr);
+
+// charAt
+let newChar = templateString.charAt(10);
+console.log(newChar);
+
+// to upper Case, lower case
+console.log(newChar.toUpperCase());
+console.log(newChar.toLowerCase());
+
+// slice and substring are same
+let subslice = templateString.slice(2, 5);
+console.log(subslice);
+
+// Return array of substring, i.e. split string into array on the basis of delimeter
+let arrString = templateString.split("a");
+console.log(arrString);
+
