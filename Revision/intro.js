5912d78a2584c2b970b1356ad5d9dae063f13647
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-29 19:23:44 +0530

    Revision intro.js

---
+++ Revision/intro.js
@@ -0,0 +1,37 @@
+// console.log is used to print anything
+console.log("Hello Everyone");
+
+// variables
+// default value -> undefined
+let a;
+// primitive types: undefined, numbers, string, boolean, null
+console.log("a", a);
+// numbers
+a = 5;
+console.log("a", a);
+a = 2.2;
+console.log("a", a);
+a = 5 / 2;
+console.log("a", a);
+
+// null
+a = null;
+console.log("a", a);
+
+// string
+a = "Hello World with double quotes";
+console.log("a", a);
+a = 'Hello World with single quotes';
+console.log("a", a);
+a = `Hello World with backtick`;
+console.log("a", a);
+
+// boolean
+a = true;
+console.log("a", a);
+
+//if else, while, for 
+for(let i = 1; i <= 10; ++ i){
+    console.log("Number is ", i);
+}
+
