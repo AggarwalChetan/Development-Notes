634244bc01ccf2f394faf0b9d104e26a69103d4b
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-30 17:51:44 +0530

    objects.js

---
+++ Revision/objects.js
@@ -0,0 +1,39 @@
+// represent
+// key -> string, number
+// values -> valid type
+// object -> key : value pair
+// object create
+
+let obj = {};
+
+let cap = {
+    // properties
+    "name" : "Steve",
+    "last Name" : "Rogers",
+    "friends" : ["Peter", "Bruce", "Tony"],
+    isAvenger : false,
+    age : 34,
+    address : {
+        state : "New York",
+        city : "NY City"
+    },
+    isAvenger : true,
+    // methods
+    sayHi : function () {
+        console.log("cap say's Hi");
+    },
+    10 : "My value is 10"   
+}
+
+
+// print 
+console.log(cap);
+
+// access
+console.log("firstName", cap.name);
+console.log("friends", cap.friends[1]);
+console.log("city", cap.address.city);
+
+
+// method call
+cap.sayHi();
