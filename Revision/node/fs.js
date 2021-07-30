f16e569eab86ea9c3425784a69673d117a0f91b9
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-30 19:20:15 +0530

    fs file functions added

--- Revision/node/fs.js
+++ Revision/node/fs.js
@@ -5,7 +5,24 @@ let fs = require('fs');
 // file -> create / delete / update / move
 // folder -> content read, create, remove
 
+// ****************file create / delete / update / move ********
 
+// read
+let content = fs.readFileSync("f1.txt");
+console.log("content :", content);
+console.log("content :" + content);
+
+// write -> writeFileSync();
+// file doesn't exist -> file create, content put
+// file does exist -> content override
+fs.writeFileSync("abc.txt", "Hello World");
+
+// append
+fs.appendFileSync("abc.txt", " in Js");
+
+// delete a file by passing it's path
+fs.unlinkSync("abc.txt");
+console.log("file removed");
 
 
 
