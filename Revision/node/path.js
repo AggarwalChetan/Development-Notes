cde7e8b97ff0748614842096d92ba7fb58bf1916
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-30 20:18:22 +0530

    path functions added

--- Revision/node/path.js
+++ Revision/node/path.js
@@ -1,1 +1,10 @@
 // to build the path
+// path 
+let path = require('path');
+
+// current directory in which command is executing
+let currentPath = process.cwd();
+console.log("currentPath", currentPath);
+
+// path join
+let joinedPath = path.join(currentPath, "abc", "def", "ghi");
