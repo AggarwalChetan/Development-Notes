d8529c78c9b6d4502f068d236bdd3078e9c07bfb
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-07-30 20:21:28 +0530

    path functions added

--- Revision/node/path.js
+++ Revision/node/path.js
@@ -8,3 +8,13 @@ console.log("currentPath", currentPath);
 
 // path join
 let joinedPath = path.join(currentPath, "abc", "def", "ghi");
+console.log(joinedPath);
+
+// baseName
+// used to get fileName from the path
+let lastPortion = path.basename(joinedPath);
+console.log(lastPortion);
+
+// path -> extension
+let ext = path.extname(joinedPath);
+console.log(ext);
