a8624a5eb327032243f766a96bfb90d11ae272db
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-08-05 22:03:09 +0530

    Js_Theory

--- JS_Theory/codeExecution.js
+++ JS_Theory/codeExecution.js
@@ -7,8 +7,35 @@
 // global area -> it is not inside any function
 
 // object given by env
-console.log(global);
+// console.log(global);
 
 // Initially this prints empty object
-console.log(this);
+// console.log(this);
+
+//Execution Context -> code + Environment(global) + JS(this)
+// Every code inside Js run in an EC
+// There are 2 phases inside an EC
+// 1. Creation phase
+    // memory allocate
+        // variables -> undefined
+        // function -> memory allocate
+
+// 2. Code Execution
+    // left to right and top to bottom execution
+
+console.log("line number", a);
+console.log("line number", b);
+var a;
+var b;
+a = 10;
+b = [1, 2, 3, 4, 5];
+console.log(a);
+console.log(b);
+fn();
+function fn(){
+    var p = 10;
+    console.log("Thank You for calling Me", p);
+}
+    
+fn();
 
