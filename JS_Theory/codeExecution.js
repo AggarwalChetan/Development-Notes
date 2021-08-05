24295b61c3a5ae7dccbcbf7cd8b45f7b6d260583
AggarwalChetan <aggarwalchetan001@gmail.com>
2021-08-05 21:39:02 +0530

    Js_Theory

---
+++ JS_Theory/codeExecution.js
@@ -0,0 +1,14 @@
+// No main 
+// js code -> environment
+// js -> browser, nodeJs
+// environment -> code + global + this
+// node -> global
+// browser -> window object
+// global area -> it is not inside any function
+
+// object given by env
+console.log(global);
+
+// Initially this prints empty object
+console.log(this);
+
